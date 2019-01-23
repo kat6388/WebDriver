64f5e86f7dcd63a1caad3ca184f2716e3fa5b049
Katya Astakhova <KatyaAstakhova@coherentsolutions.com>
2019-01-23 16:11:18 +0300

    Task20

---
+++ Task20/ClassLibrary1/Tests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        public const string url = "https://www.tut.by/";
+        public const string username = "seleniumtests@tut.by";
+        public const string password = "123456789zxcvbn";
+
+        [Test]
+        public void Login()
+        {
+
+            var driver = Drivers.Methods.NavigateTo(url);
+            var actualUrl = Drivers.Methods.GetUrl(driver);
+            Assert.IsTrue(actualUrl.StartsWith(url), "invalid url)");
+
+            Drivers.Methods.PopulateLoginForm(driver,username,password);
+
+            var submitButtonState = Drivers.Methods.IsLoginButtonEnabled(driver);
+            Drivers.Methods.IsLoginButtonEnabled(driver);
+            Assert.IsTrue(submitButtonState);
+
+            Drivers.Methods.LoginUser(driver);
+
+            var loginState = Drivers.Methods.IsUserLogin(driver);
+            Drivers.Methods.IsUserLogin(driver);
+            Assert.IsTrue(loginState);
+        }
+    }
+}
---
+++ Task20/ClassLibrary2/Methods.cs
@@ -0,0 +1,45 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+
+namespace Drivers
+{
+    public class Methods
+    {
+        public static IWebDriver NavigateTo(string url)
+        {
+            var chromeDriver = new ChromeDriver(@"D:\Automation");
+            chromeDriver.Navigate().GoToUrl(url);
+            return chromeDriver;
+        }
+
+        public static string GetUrl(IWebDriver chromeDriver)
+        {
+            return chromeDriver.Url;
+        }
+
+        public static void PopulateLoginForm(IWebDriver chromeDriver, string username, string password)
+        {
+            Actions builder = new Actions(chromeDriver);
+            chromeDriver.FindElement(By.LinkText("Войти")).Click();
+            chromeDriver.FindElement(By.Name("login")).SendKeys(username);
+            builder.SendKeys(Keys.Tab).Perform();
+            chromeDriver.FindElement(By.Name("password")).SendKeys(password);
+        }
+
+        public static bool IsLoginButtonEnabled(IWebDriver chromeDriver)
+        {
+            return chromeDriver.FindElement(By.ClassName("auth__enter")).Enabled;
+        }
+
+        public static void LoginUser(IWebDriver chromeDriver)
+        {
+            chromeDriver.FindElement(By.ClassName("auth__enter")).Click();
+        }
+
+        public static bool IsUserLogin(IWebDriver chromeDriver)
+        {
+            return chromeDriver.FindElement(By.LinkText("Selenium Test")).Displayed;
+        }
+    }
+}
